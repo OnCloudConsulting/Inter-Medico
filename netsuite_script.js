7229edfe5bee3d745fa8dd775f8428e31f9302b9
DhruvSoni1706 <234250585+OnCloudConsulting@users.noreply.github.com>
2026-08-31 15:46:30 -0400

    Update from NetSuite (Auto)

--- netsuite_script.js
+++ netsuite_script.js
@@ -2,7 +2,7 @@
   "id": "",
   "name": "Unknown",
   "recordType": "",
-  "exportedAt": "2026-08-31T19:44:45.778Z",
+  "exportedAt": "2026-08-31T19:46:28.446Z",
   "criteria": [],
   "columns": [],
   "sort": []
